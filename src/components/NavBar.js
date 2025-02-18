f3c939b13fff783069744bca90136786aa019d15
Nikita Levin <levin.nikitaa@gmail.com>
2025-02-19 00:41:43 +0200

    added navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,62 @@
+import React from "react";
+import styled from "styled-components";
+import logoImg from '../images/logo.svg';
+import signImg from '../images/sign.svg';
+
+const NavBarStyled = styled.header`
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 999;
+    height: 80px;
+    width: 100vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    background-color: #299B01;
+    color: white;|
+`;
+
+const Logo = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const H1 = styled.h1`
+    font-size: 24px;
+    margin-left: 15px;
+`;
+
+const ImgLogo = styled.img`
+    width: 50px;
+`;
+
+const SignInBtn = styled.button`
+    margin-right: 1.5vw;
+    border: transparent; 
+    background-color: transparent;
+    color: white;
+    font-size: 16px;
+`;
+
+const ImgSign = styled.img`
+    align-items: center;
+    width: 30px;
+    height: 30px;
+`;
+
+
+
+export const NavBar = () => (
+    <NavBarStyled>
+        <Logo>
+            <ImgLogo src = {logoImg} alt="logo"/>
+            <H1>Mr Donald's</H1>
+        </Logo>
+        <SignInBtn>
+            <ImgSign src = {signImg} alt="Sign In Img" />
+            <p>Sign in</p>
+        </SignInBtn>
+    </NavBarStyled>
+);
