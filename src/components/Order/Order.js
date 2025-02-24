225d5cfd0b808392a29786ee7187bd8751adebca
Nikita Levin <levin.nikitaa@gmail.com>
2025-02-24 20:15:14 +0200

    feat: deleting items from order

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -47,7 +47,14 @@ const EmptyList = styled.p`
     text-align: center;
 `;
 
-export const Order = ({ orders }) => {
+export const Order = ({ orders, setOrders }) => {
+
+    const deleteItem = index => {
+        const newOrders = [...orders];
+
+        newOrders.splice(index, 1);
+        setOrders(newOrders);
+    }
 
     const total = orders.reduce((result, order) =>
         totalPriceItems(order) + result, 0)
@@ -61,7 +68,12 @@ export const Order = ({ orders }) => {
             <OrderContent>
                 {orders.length ?
                 <Orderlist>
-                    {orders.map(order => <OrderListItem order={order}/>)}
+                    {orders.map((order, index) => <OrderListItem
+                        key={index}
+                        order={order}
+                        deleteItem={deleteItem}
+                        index={index}
+                        />)}
                 </Orderlist> :
                 <EmptyList>Your cart is empty</EmptyList>}
             </OrderContent>
